40b8e60c2832da7c5aa3e8ef3039c96ca31999e0
Aniello Sgambato <aasgambato@gmail.com>
2025-11-12 12:54:22 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,18 +1,24 @@
+
+"use client"
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState<boolean>(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    const checkIsMobile = () => {
+        setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    
+    // Check on mount
+    checkIsMobile();
+
+    window.addEventListener("resize", checkIsMobile)
+    
+    return () => window.removeEventListener("resize", checkIsMobile)
   }, [])
 
   return isMobile
