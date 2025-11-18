3a331c97a30e2c57f469954aa37340f2e685e4d5
Aniello Sgambato <aasgambato@gmail.com>
2025-11-18 14:06:26 +0000

    sul desktop funziona tutto, su mobile continua a non attivarsi l'azione

--- src/app/(main)/meals/page.tsx
+++ src/app/(main)/meals/page.tsx
@@ -50,8 +50,8 @@ export default function MealsPage() {
   const sensors = useSensors(
     useSensor(PointerSensor, {
       activationConstraint: {
-        // Drag is only initiated after a pointer travels 10px
-        distance: 10,
+        delay: 250,
+        tolerance: 5,
       },
     }),
     useSensor(KeyboardSensor, {
@@ -159,7 +159,7 @@ export default function MealsPage() {
 
         <div className="py-4 pt-8 space-y-4">
           {filteredMeals.length > 0 ? (
-             <SortableContext items={filteredMeals.map(m => m.id)} strategy={isCollapsed ? verticalListSortingStrategy : rectSortingStrategy} disabled={!isReorderable}>
+             <SortableContext items={filteredMeals.map(m => m.id)} strategy={isCollapsed ? verticalListSortingStrategy : rectSortingStrategy} disabled={!isReorderable && isMobile}>
                 <div className={cn(
                     "grid gap-4",
                     isCollapsed ? "grid-cols-1" : "grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
